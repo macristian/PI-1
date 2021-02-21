81bae6733885b513f3ca71f7e223deb722b72e2b
Cristian Bernardo <cristian.silva@ceca.ufal.br>
2021-02-21 11:47:13 -0300

    Melhorias e Correções

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -4,9 +4,9 @@ namespace Scisense.Models
 {
     public abstract class Repository
     {
-        //protected const string _strConexao = "Database = Scisense; Data Source = localhost; User Id = root; Password = ";
+        protected const string _strConexao = "Database = Scisense; Data Source = localhost; User Id = root; Password = ";
         
-        protected const string _strConexao = "Server=MYSQL5043.site4now.net;Database=db_a6fb52_sci;Uid=a6fb52_sci;Pwd=Sc!Sense142536";
+        //protected const string _strConexao = "Server=MYSQL5043.site4now.net;Database=db_a6fb52_sci;Uid=a6fb52_sci;Pwd=Sc!Sense142536";
         protected MySqlConnection conexao = new MySqlConnection(_strConexao);
     }
 }
